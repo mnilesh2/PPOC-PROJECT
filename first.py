a6bfe1d654829f80af89629792ee411ed31251cc
mnilesh2 <128702602+mnilesh2@users.noreply.github.com>
2023-03-23 21:25:16 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sum = 0 
+for i in range(1,11):
+    sum += i
+print("the sum of the first 10 integer is:",sum)
+
+
+# In[12]:
+
+
+#define a funtion that takes two arguments and return their sum 
+def add_numbers(x,y):
+    return x + y
+#now print the fn
+result = add_numbers(7, 8)
+#you can take any number
+print(result)
+
+
+# In[16]:
+
+
+def name_rollno(fname):
+    print(fname)
+    
+name_rollno("(220715,Nilesh)")
+
+
+# In[ ]:
+
+
+
+
